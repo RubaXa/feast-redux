89a459b0cffb4eead2ccf692b2e75546570e27db
Лебедев Константин <k.lebedev@corp.mail.ru>
2017-03-31 19:33:26 +0300

    * es2015+

--- examples/todomvc/containers/App.js
+++ examples/todomvc/containers/App.js
@@ -1,24 +1,23 @@
-import feast from 'feast'
+import feast, { Block } from 'feast'
 import { bindActionCreators } from 'redux'
 import { connect } from 'feast-redux'
 import Header from '../components/Header'
 import MainSection from '../components/MainSection'
 import * as TodoActions from '../actions'
 
-const App = feast.Block.extend({
-  name: 'app',
-  performanceTrace: true,
-  
-  blocks: {
+class App extends Block {
+  static blockName = 'app'
+
+  static blocks = {
     'header': Header,
     'main-section': MainSection
-  },
+  }
   
-  template: `<div>
+  static template = `<div>
     <b:header add-todo="{attrs.actions.addTodo}" />
     <b:main-section todos="{attrs.todos}" actions="{attrs.actions}" />
   </div>`
-});
+}
 
 function mapStateToAttrs(state) {
   return {
