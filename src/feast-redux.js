5b6d2cec428bcd7f6586e8d2b32fe3d3ffb5e6bb
Лебедев Константин <k.lebedev@corp.mail.ru>
2017-06-06 17:53:32 +0300

    v0.4.0: HOC

--- src/feast-redux.js
+++ src/feast-redux.js
@@ -28,39 +28,40 @@ export function connect(mapStateToAttrs, mapDispatchToAttrs, mergeAttrs) {
 	}
 
 	return function ConnectDecorator(UIFeastClass) {
-		const _super = UIFeastClass.prototype;
-
-		return UIFeastClass.extend({
-			name: `${UIFeastClass.blockName || UIFeastClass.prototype.name}Connect`,
-
+		const HOC = class ReduxConnectDecorator extends UIFeastClass {
 			constructor(attrs, context) {
-				_super.constructor.call(this, prepareAttrs(attrs, context.store), context);
-				this.unsibscribeHandleReduxStoreChanged = context.store.subscribe(this.handleReduxStoreChanged.bind(this));
-				
-				const eventsToActions = this.eventsToActions || {};
+				super(prepareAttrs(attrs, context.store), context);
+
+				const eventsToActions = (this.constructor.eventsToActions || this.eventsToActions || {});
 
 				Object.keys(eventsToActions).forEach((eventName) => {
 					this.on(eventName, (evt) => {
 						const decl = eventsToActions[eventName];
 						const args = decl.arguments ? [].concat(decl.arguments(this, evt)) : [];
-						
+
 						this.context.store.dispatch(decl.action.apply(null, args));
 					});
 				});
-			},
 
-			set(attrs) {
-				_super.set.call(this, prepareAttrs(attrs, this.context.store));
-			},
+				this.unsibscribeHandleReduxStoreChanged = context.store.subscribe(this.handleReduxStoreChanged.bind(this));
+			}
 
 			handleReduxStoreChanged() {
 				this.set({});
-			},
+			}
+
+			set(attrs) {
+				super.set(prepareAttrs(attrs, this.context.store));
+			}
 
 			destroy() {
-				_super.destroy.apply(this, arguments);
+				super.destroy.apply(this, arguments);
 				this.unsibscribeHandleReduxStoreChanged();
 			}
-		});
+		};
+
+		HOC.prototype.name = `${UIFeastClass.blockName || UIFeastClass.prototype.name}Connect`;
+
+		return HOC;
 	}
 }
